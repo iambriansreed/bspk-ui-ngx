2a7b5162b51ef044fb88e8dc456079e610cc3b53
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-12-30 11:40:13 -0600

    chore(Divider): [BSPK-129] add examples (#94)

--- projects/ui/src/lib/divider/example.ts
+++ projects/ui/src/lib/divider/example.ts
@@ -11,35 +11,105 @@ import { UIDivider } from './divider';
     template: `
         <h3>Divider</h3>
 
-        <h4>Horizontal light (default)</h4>
+        <h4>Default</h4>
         <ui-example width="320px" direction="column">
             <ui-list-item [label]="'Above'"></ui-list-item>
             <ui-divider />
             <ui-list-item [label]="'Below'"></ui-list-item>
         </ui-example>
 
-        <h4>Horizontal light (default), padding = false</h4>
+        <h4>orientation = horizontal (default), thickness = light (default), padding = false</h4>
         <ui-example width="320px" direction="column">
             <ui-list-item [label]="'Above'"></ui-list-item>
-            <ui-divider [padding]="false" />
+            <ui-divider orientation="horizontal" [padding]="false" [thickness]="'light'" />
             <ui-list-item [label]="'Below'"></ui-list-item>
         </ui-example>
 
-        <h4>Horizontal, thickness = heavy, padding = false, inset = 2</h4>
+        <h4>thickness = heavy, padding = false</h4>
+        <ui-example width="320px" direction="column">
+            <ui-list-item [label]="'Above'"></ui-list-item>
+            <ui-divider [thickness]="'heavy'" [padding]="false" />
+            <ui-list-item [label]="'Below'"></ui-list-item>
+        </ui-example>
+
+        <h4>inset = 4</h4>
+        <ui-example width="320px" direction="column">
+            <ui-list-item [label]="'Above'"></ui-list-item>
+            <ui-divider [inset]="4" />
+            <ui-list-item [label]="'Below'"></ui-list-item>
+        </ui-example>
+
+        <h4>thickness = heavy, inset = 4</h4>
+        <ui-example width="320px" direction="column">
+            <ui-list-item [label]="'Above'"></ui-list-item>
+            <ui-divider [inset]="4" [thickness]="'heavy'" />
+            <ui-list-item [label]="'Below'"></ui-list-item>
+        </ui-example>
+
+        <h4>padding = false, inset = 6</h4>
+        <ui-example width="320px" direction="column">
+            <ui-list-item [label]="'Above'"></ui-list-item>
+            <ui-divider [inset]="6" [padding]="false" />
+            <ui-list-item [label]="'Below'"></ui-list-item>
+        </ui-example>
+
+        <h4>thickness = heavy, padding = false, inset = 2</h4>
         <ui-example width="320px" direction="column">
             <ui-list-item [label]="'Above'"></ui-list-item>
             <ui-divider [thickness]="'heavy'" [inset]="2" [padding]="false" />
             <ui-list-item [label]="'Below'"></ui-list-item>
         </ui-example>
 
-        <h4>Vertical, inset = 2</h4>
+        <h4>orientation = vertical</h4>
+        <ui-example>
+            <ui-list-item [label]="'Left'"></ui-list-item>
+            <ui-divider [orientation]="'vertical'" />
+            <ui-list-item [label]="'Right'" style="background: blue;"></ui-list-item>
+        </ui-example>
+
+        <h4>orientation = vertical, thickness = heavy</h4>
+        <ui-example>
+            <ui-list-item [label]="'Left'"></ui-list-item>
+            <ui-divider [orientation]="'vertical'" [thickness]="'heavy'" />
+            <ui-list-item [label]="'Right'" style="background: blue;"></ui-list-item>
+        </ui-example>
+
+        <h4>orientation = vertical, padding = false</h4>
+        <ui-example>
+            <ui-list-item [label]="'Left'"></ui-list-item>
+            <ui-divider [orientation]="'vertical'" [padding]="false" />
+            <ui-list-item [label]="'Right'" style="background: blue;"></ui-list-item>
+        </ui-example>
+
+        <h4>orientation = vertical, thickness = heavy, padding = false</h4>
+        <ui-example>
+            <ui-list-item [label]="'Left'"></ui-list-item>
+            <ui-divider [orientation]="'vertical'" [padding]="false" [thickness]="'heavy'" />
+            <ui-list-item [label]="'Right'" style="background: blue;"></ui-list-item>
+        </ui-example>
+
+        <h4>orientation = vertical, padding = false, inset = 4</h4>
+        <ui-example>
+            <ui-list-item [label]="'Left'"></ui-list-item>
+            <ui-divider [orientation]="'vertical'" [padding]="false" [inset]="4" />
+            <ui-list-item [label]="'Right'" style="background: blue;"></ui-list-item>
+        </ui-example>
+
+        <h4>orientation = vertical, thickness = heavy, padding = false, inset = 4</h4>
+        <ui-example>
+            <ui-list-item [label]="'Left'"></ui-list-item>
+            <ui-divider [orientation]="'vertical'" [padding]="false" [thickness]="'heavy'" [inset]="4" />
+            <ui-list-item [label]="'Right'" style="background: blue;"></ui-list-item>
+        </ui-example>
+
+        <h4>orientation = vertical, inset = 2</h4>
         <ui-example>
             <ui-list-item [label]="'Left'"></ui-list-item>
             <ui-divider [orientation]="'vertical'" [inset]="2" />
             <ui-list-item [label]="'Right'" style="background: blue;"></ui-list-item>
         </ui-example>
 
-        <h4>Vertical, thickness = heavy, inset = 2</h4>
+        <h4>orientation = vertical, thickness = heavy, inset = 2</h4>
         <ui-example>
             <ui-list-item [label]="'Left'"></ui-list-item>
             <ui-divider [orientation]="'vertical'" [thickness]="'heavy'" [inset]="2" />
