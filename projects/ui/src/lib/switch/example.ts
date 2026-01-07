72d71118c3a670d883d9caa24dfb6e161fc71766
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2026-01-07 16:22:30 -0600

    chore(Switch): update examples to trigger snackbar on change (#108)
    * chore(Switch): update examples to trigger snackbar on change
    
    * chore(SwitchOption): update basic example to have no props preset
    
    * chore(SwitchOption): update examples to remove invalid and required props not present on the component

--- projects/ui/src/lib/switch/example.ts
+++ projects/ui/src/lib/switch/example.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { sendSnackbar } from '../../utils/sendSnackbar';
 import { UISwitch } from './switch';
 
 @Component({
@@ -8,26 +9,69 @@ import { UISwitch } from './switch';
     imports: [CommonModule, UISwitch],
     template: `
         <h2>Switch</h2>
-
-        <h3>Basic with id</h3>
-        <ui-switch id="test-id" name="switch-0" ariaLabel="Switch 0" value="Switch 0"></ui-switch>
-
-        <h3>checked = true</h3>
-        <ui-switch name="switch-1" ariaLabel="Switch 1" value="Switch 1" [checked]="true"></ui-switch>
-
-        <h3>checked = false</h3>
-        <ui-switch name="switch-2" ariaLabel="Switch 2" value="Switch 2" [checked]="false"></ui-switch>
-
-        <h3>checked = true, disabled = true</h3>
-        <ui-switch name="switch-3" ariaLabel="Switch 3" value="Switch 3" [checked]="true" [disabled]="true"></ui-switch>
-
-        <h3>checked = false, disabled = true</h3>
-        <ui-switch
-            name="switch-4"
-            ariaLabel="Switch 4"
-            value="Switch 4"
-            [checked]="false"
-            [disabled]="true"></ui-switch>
+        @for (ex of examples; track $index) {
+            <h3>{{ ex.label }}</h3>
+            <ui-switch
+                [id]="ex.id"
+                [name]="ex.name"
+                [ariaLabel]="ex.ariaLabel"
+                [value]="ex.value"
+                [checked]="ex.checked"
+                [disabled]="ex.disabled"
+                (checkedChange)="onCheckedChange($index, $event)">
+            </ui-switch>
+            <div>Demo checked: {{ ex.checked }}</div>
+        }
     `,
 })
-export class UISwitchExample {}
+export class UISwitchExample {
+    examples = [
+        {
+            label: 'Basic with id',
+            id: 'test-id',
+            name: 'switch-0',
+            ariaLabel: 'Switch 0',
+            value: 'Switch 0',
+            checked: false,
+        },
+        {
+            label: 'checked = true',
+            id: 'switch-1',
+            name: 'switch-1',
+            ariaLabel: 'Switch 1',
+            value: 'Switch 1',
+            checked: true,
+        },
+        {
+            label: 'checked = false',
+            id: 'switch-2',
+            name: 'switch-2',
+            ariaLabel: 'Switch 2',
+            value: 'Switch 2',
+            checked: false,
+        },
+        {
+            label: 'checked = true, disabled = true',
+            id: 'switch-3',
+            name: 'switch-3',
+            ariaLabel: 'Switch 3',
+            value: 'Switch 3',
+            checked: true,
+            disabled: true,
+        },
+        {
+            label: 'checked = false, disabled = true',
+            id: 'switch-4',
+            name: 'switch-4',
+            ariaLabel: 'Switch 4',
+            value: 'Switch 4',
+            checked: false,
+            disabled: true,
+        },
+    ];
+
+    onCheckedChange(idx: number, checked: boolean) {
+        this.examples[idx].checked = checked;
+        sendSnackbar(`Switch ${idx} checked: ${checked}`);
+    }
+}
