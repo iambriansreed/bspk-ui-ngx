c146bb4a7cb0feda1d3e25802165320cefc92ca6
Emile Fleming <emile.fleming@anywhere.re>
2026-01-28 10:59:23 -0500

    chore: update test helper import to new format

--- projects/ui/src/lib/input-phone/input-phone.spec.ts
+++ projects/ui/src/lib/input-phone/input-phone.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { hasNoBasicA11yIssues } from '../../testutils/hasNoBasicA11yIssues';
+import { hasNoBasicA11yIssues } from '@shared/testing/hasNoBasicA11yIssues';
 import { UIInputPhone } from './input-phone';
 
 describe('UIInputPhone', () => {
