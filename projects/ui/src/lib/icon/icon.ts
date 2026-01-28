1b29bb3cd50c870303c5856863b0158bcb28f60e
anybrian <brian.reed@anywhere.re>
2026-01-28 15:44:05 -0500

    fix: handle icon props update

--- projects/ui/src/lib/icon/icon.ts
+++ projects/ui/src/lib/icon/icon.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, inject, ViewContainerRef, AfterViewInit, input } from '@angular/core';
+import { Component, ViewEncapsulation, inject, ViewContainerRef, input, effect, ComponentRef } from '@angular/core';
 import { BspkIcon } from '../../types/bspk-icon';
 
 @Component({
@@ -7,19 +7,27 @@ import { BspkIcon } from '../../types/bspk-icon';
     styles: 'ui-icon { display: contents; }',
     encapsulation: ViewEncapsulation.None,
 })
-export class UIIcon implements AfterViewInit {
+export class UIIcon {
     /** @see https://bspk.anywhere.re/icons */
     readonly icon = input.required<BspkIcon>();
     readonly width = input<string>();
-
     viewContainerRef = inject(ViewContainerRef);
 
-    ngAfterViewInit() {
-        const iconValue = this.icon();
-        if (typeof iconValue !== 'function') return;
+    componentRef?: ComponentRef<any>;
+    currentIcon?: BspkIcon;
 
-        const icon = this.viewContainerRef.createComponent(iconValue).instance;
+    constructor() {
+        effect(() => {
+            const nextIcon = this.icon();
+            if (typeof nextIcon !== 'function' || this.currentIcon === nextIcon) return;
 
-        icon.width = this.width();
+            this.viewContainerRef.clear();
+            this.componentRef = this.viewContainerRef.createComponent(nextIcon);
+            this.currentIcon = nextIcon;
+        });
+
+        effect(() => {
+            if (this.componentRef) this.componentRef.instance.width = this.width();
+        });
     }
 }
