e90badeb104b9d6bae85d7ddce7b475b8b2ea66a
anybrian <brian.reed@anywhere.re>
2026-01-28 22:26:42 -0500

    fix: handle icon props update (#141)
    * fix: handle icon props update
    
    * examples save the day!
    
    * cleanup

--- projects/ui/src/lib/icon/example.ts
+++ projects/ui/src/lib/icon/example.ts
@@ -1,25 +1,61 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { IconIcecream } from '../icons/icecream';
+import { Component, computed, model } from '@angular/core';
+import { IconCat, IconDog, IconIcecream, UIRadioGroup } from '../..';
+import { BspkIcon } from '../../types/bspk-icon';
 import { UIIcon } from './icon';
 
 @Component({
     selector: 'ui-icon-example',
     standalone: true,
-    imports: [CommonModule, UIIcon],
+    imports: [CommonModule, UIIcon, UIRadioGroup],
     template: `
         <h4>Using prop width = 24</h4>
         <div>
-            <ui-icon [icon]="iconIcecream" width="24"> </ui-icon>
+            <ui-icon [icon]="iconIcecream" width="24" />
         </div>
 
         <h4>Not using width prop</h4>
         <p>Parent div has a width of 50px</p>
         <div style="width: 50px">
-            <ui-icon [icon]="iconIcecream"> </ui-icon>
+            <ui-icon [icon]="iconIcecream" />
+        </div>
+
+        <h4>Change icon</h4>
+        <div>
+            <ui-icon [icon]="iconCurrent()" [width]="iconWidth()" />
+        </div>
+
+        <div style="margin-top: 16px; width: 100px">
+            <ui-radio-group
+                name="select-icon-name"
+                label="Select Icon"
+                (valueChange)="iconName.set($event)"
+                [value]="iconName()"
+                [options]="[
+                    { label: 'Cat', value: 'cat' },
+                    { label: 'Dog', value: 'dog' },
+                ]" />
+        </div>
+
+        <div style="margin-top: 16px; width: 100px">
+            <ui-radio-group
+                name="select-icon-width"
+                label="Select Icon Width"
+                (valueChange)="iconWidth.set($event)"
+                [value]="iconWidth()"
+                [options]="[
+                    { label: '50px', value: '50px' },
+                    { label: '100px', value: '100px' },
+                ]" />
         </div>
     `,
 })
 export class UIIconExample {
-    protected readonly iconIcecream = IconIcecream;
+    iconIcecream = IconIcecream;
+    iconCat = IconCat;
+    iconDog = IconDog;
+
+    readonly iconCurrent = computed<BspkIcon>(() => (this.iconName() === 'cat' ? this.iconCat : this.iconDog));
+    readonly iconName = model('cat');
+    readonly iconWidth = model('50px');
 }
