f4bd99e32e514607d969d401ad61c41b1c40dd82
anybrian <brian.reed@anywhere.re>
2026-02-06 17:45:52 -0500

    fix: version update

--- .scripts/all.ts
+++ .scripts/all.ts
@@ -6,15 +6,48 @@ import { execSync as execSyncBase } from 'child_process';
 import chalk from 'chalk';
 import fs from 'fs';
 
+const SCRIPTS = {
+    /** Generates the metadata file used for documentation generation. */
+    meta() {
+        if (args.includes('f') || !fs.existsSync('.tmp/documentation.json'))
+            exec('npx @compodoc/compodoc -p tsconfig.doc.json -e json -d ./.tmp');
+
+        exec('npx tsx .scripts/generate-meta.ts --write');
+    },
+
+    'update-version'() {
+        // read the latest tag from git
+        exec('git fetch --tags');
+        const latestTag = execSyncBase('git describe --tags').toString().trim().substring(1); // remove the leading 'v' from the tag
+
+        // update the version in package.json and projects/ui/package.json to match the latest tag
+        const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+        const uiPackageJson = JSON.parse(fs.readFileSync('projects/ui/package.json', 'utf-8'));
+
+        packageJson.version = latestTag;
+        uiPackageJson.version = latestTag;
+
+        fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+        fs.writeFileSync('projects/ui/package.json', JSON.stringify(uiPackageJson, null, 2));
+
+        // update the version in package.json and projects/ui/package.json to match the latest tag
+
+        exec('git add package.json projects/ui/package.json');
+
+        // commit the changes and push to the repo
+
+        exec(`git commit -m "chore: update version to ${latestTag} [skip ci]"`);
+        exec('git push');
+    },
+};
+
 const { cyan, yellow } = chalk;
 
 const exec = (command: string) => execSyncBase(command, { stdio: 'inherit' });
 
 const args = process.argv.slice(3);
 
-const scriptDictionary: Record<string, () => void> = { meta };
-
-const nextScript = scriptDictionary[process.argv[2]];
+const nextScript = process.argv[2] in SCRIPTS ? SCRIPTS[process.argv[2] as keyof typeof SCRIPTS] : null;
 
 if (nextScript) {
     console.log(yellow(`${'Running script:'} ${cyan(process.argv[2])} with args: ${cyan(args.join(', ') || 'none')}`));
@@ -23,13 +56,4 @@ if (nextScript) {
 } else {
     console.error(`No script found for ${process.argv[2]}`);
     process.exit(1);
-}
-
-// Script implementations
-
-function meta() {
-    if (args.includes('f') || !fs.existsSync('.tmp/documentation.json'))
-        exec('npx @compodoc/compodoc -p tsconfig.doc.json -e json -d ./.tmp');
-
-    exec('npx tsx .scripts/generate-meta.ts --write');
 }
